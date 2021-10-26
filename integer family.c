2c99b2a19f42d0eb429e9bf469dbaca9c9b675c9
LikhithaMaddala <92352677+LikhithaMaddala@users.noreply.github.com>
2021-10-26 12:23:25 +0530

    Add files via upload

---
+++ integer family.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main()
+{
+	//short int
+	signed short int a;
+	a=8-7;
+	printf("%hd\n",a);
+	
+	unsigned short int b;
+	b=8-7;
+	printf("%hu\n",b);
+	//int
+	signed int c;
+	c=8-7;
+	printf("%d\n",c);
+	
+	unsigned int d;
+	d=8-7;
+	printf("%u\n",d);
+	//long int
+	signed long int e;
+	e=8-7;
+	printf("%ld\n",e);
+	
+	unsigned long int f;
+	f=8-7;
+	printf("%lu\n",f);
+	//long long int
+	signed long long int g;
+	g=8-7;
+	printf("%lld\n",g);
+	
+	unsigned long long int h;
+	h=8-7;
+	printf("%llu",h);
+}
+
