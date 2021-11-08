503a5891242b3468960753b8a12dff51b34297b4
LikhithaMaddala <92352677+LikhithaMaddala@users.noreply.github.com>
2021-11-08 12:34:05 +0530

    Add files via upload

---
+++ comparision operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int A,B,C,D,E,F;
+	A=20>10;
+	B=20>=10;
+	C=20<10;
+	D=20<=10;
+    E=20==10;
+    F=20!=10;
+	printf("20 > 10 --> %d\n",A);
+	printf("20 >= 10 --> %d\n",B);
+	printf("20 < 10 --> %d\n",C);
+	printf("20 <= 10 --> %d\n",D);
+	printf("20 == 10 --> %d\n",E);
+	printf("20 != 10 --> %d",F);
+	
+}
