03a22f6cbf9dcbed5f41c2adcea849aa9de8c7a9
LikhithaMaddala <92352677+LikhithaMaddala@users.noreply.github.com>
2021-11-08 12:32:19 +0530

    Delete Boolean values.c

--- Boolean values.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	printf("%d\n", (80>70) && (80>7));
-	printf("%d\n", (80>70) && (80<7));
-	printf("%d\n", (80>70) || (80>7));
-	printf("%d", (80<70) || (80<7));	
-}
